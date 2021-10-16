ad0ea6db27a6347d74ee80a4d5ee11966d9131d9
RinHizakura <s921975628@gmail.com>
2021-10-16 19:56:00 +0800

    Done the simple template to run rust on android
    Now we can return a string from rust to java(android)!

--- android/app/src/main/java/com/example/android/MainActivity.java
+++ android/app/src/main/java/com/example/android/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    static {
+        System.loadLibrary("greetings");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RustUtils g = new RustUtils();
+        String r = g.sayHello("Amita");
+        ((TextView)findViewById(R.id.textView)).setText(r);
     }
 }
